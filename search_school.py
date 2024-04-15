1de872ea19dc5a4f15d3ee6c2c8c502d9ab3c128
Andrey <139971881+Prockofi@users.noreply.github.com>
2024-04-15 17:27:38 +0300

    Поиск школы

--- search_school.py
+++ search_school.py
@@ -1,36 +1,35 @@
-import requests, json
+import requests, json, re
 
-#Принимает название школы и url электронного дневника
+# Принимает название школы и url электронного дневника
 def search_school(name_school: str, url: str) -> str:
-    responce = requests.get(f'{url}/webapi/schools/search').json()
+    responce = requests.get(f"{url}/webapi/schools/search").json()
     res = []
-    name_school = name_school.lower()
-    name_school = (name_school.replace('№', '№ '))
-    name_school = (name_school.replace('сош', ' сош '))
+    name_school = (name_school.replace("№", " № "))
+    name_school = (name_school.replace("сош", " сош "))
+    
+    while "  " in name_school:
+        name_school = name_school.replace("  ", " ")
 
-    while '  ' in name_school:
-        name_school = name_school.replace('  ', ' ')
+    try:
+        num = re.findall("\d+", name_school)[0]
+    
+    except:
+        num = 0
 
     name_school = name_school.split()
-    for word in name_school:
-        try: 
-            num = int(word)
-            break
-        except:
-            pass
-    else:
-        num = None
+    
     for word in name_school:
         for line in responce:
-            if word.lower() in line['name'].lower():
-                res.append(line['shortName'])
-    schools = ''
+            if word in line["name"].lower():
+                res.append(line["shortName"])
+    
+    school = None
     max_count = 0
     for el in res:
         if res.count(el) > max_count:
             if num != None:
                 if str(num) in el:
-                    max_count = res.count(el)
+                    max_count = res.count(el) + 3
                     school = el
             else:
                 max_count = res.count(el)
