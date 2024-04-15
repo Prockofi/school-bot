356b657f581eaf9417687100ea0cfc3e5b098461
Andrey <139971881+Prockofi@users.noreply.github.com>
2024-04-15 17:26:42 +0300

    Получение данных

--- get_netschool.py
+++ get_netschool.py
@@ -3,22 +3,22 @@ from netschoolapi import NetSchoolAPI
 from DataBase.database import lessons
 
 class LessonsClass:
-    async def main(self, login: str, password: str, school: str, school_url: str) -> str:
-        connect = NetSchoolAPI(school_url)
+    async def main(self, login: str, password: str, school: str, url: str) -> str:
+        connect = NetSchoolAPI(url)
         await connect.login(login, password, school)
         get_data = await connect.diary(start=datetime.date(2023, 9, 1), end=datetime.date(2024, 5, 31))
         await connect.logout()
 
-        get_data = str(get_data).split('Day')
+        get_data = str(get_data).split("Day")
         year = {}
         for get_day in get_data:
             day = []
-            for el in get_day.split('Lesson('):
+            for el in get_day.split("Lesson("):
                 if len(str(el)) > 10:
                     line = ''
-                    index = el.find(', mark=')
-                    if el.count('mark') == 2 and (el[index+7:index+8] in 'N')  and (el[index+7:index+8] != ''):
-                        index = el.find(', mark=', index+50)
+                    index = el.find(", mark=")
+                    if el.count("mark") == 2 and (el[index+7:index+8] in 'N')  and (el[index+7:index+8] != ''):
+                        index = el.find(", mark=", index+50)
                     if index != -1:
                         if (el[index+7:index+8] in ['1', '2', '3', '4', '5']) and (el[index+7:index+8] != ''):
                             line += el[index+7:index+8]
@@ -26,11 +26,11 @@ class LessonsClass:
                             line += '6'
                     else:
                         line = '6' + line
-                    index = el.find('subject=')
+                    index = el.find("subject=")
                     if index != -1:
                         lesson = el[index+8:index+40].split("'")
                         line += ' ' + lesson[1]
-                    index = el.find('day=datetime.date(')
+                    index = el.find("day=datetime.date(")
                     if index != -1:
                         if ', ' == el[index+29:index+31]:
                             time = el[index+17:index+29]
@@ -46,14 +46,14 @@ class LessonsClass:
             write_data += '$' + day + '#'.join(year.get(day))
         return str(write_data)
 
-    async def get_data(self, user_id: int, login: str, password: str, school: str, school_url: str) -> list:
-        year = await self.main(login, password, school, school_url)
+    async def get_data(self, user_id: int, login: str, password: str, school: str, url: str) -> list:
+        year = await self.main(login, password, school, url)
         try:
             read_data = (await lessons.get_not_empty(user_id))[1]
         except:
-            read_data = 'None'
-        await lessons.get_user_id(user_id)
-        await lessons.enter(user_id, ['lessons', year])
+            read_data = "None"
+        await lessons.user_id(user_id)
+        await lessons.enter(user_id, ["lessons", year])
         return await self.compare((await self.filter(year)).split('#'), (await self.filter(read_data)).split('#')), year
 
     async def compare(self, new_data: str, old_data: str) -> list:
